7f1f0302b498de49f4a14d4ea0a65629780aeb77
tharun <tharun.damera4@gmail.com>
2025-11-10 13:01:23 +0530

    Add comments in error and message rs files

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -6,6 +6,7 @@ use serde_json::Error as SerdeJsonError;
 use solana_client::client_error::ClientError;
 use solana_sdk::pubkey::ParsePubkeyError;
 
+// Create an AppError using thiserror that handles almost all errors
 #[derive(Error, Debug)]
 pub enum AppError {
     #[error("Database Error: {0}")]
@@ -24,42 +25,49 @@ pub enum AppError {
     BadRequestError(String),
 }
 
+// Map the sqlx::Error to the DatabaseError variant of the AppError
 impl From<SqlxError> for AppError {
     fn from(e: SqlxError) -> Self {
         AppError::DatabaseError(e.to_string())
     }
 }
 
+// Map the sqlx::migrate::MigrateError to the DatabaseError variant of the AppError
 impl From<MigrateError> for AppError {
     fn from(e: MigrateError) -> Self {
         AppError::DatabaseError(e.to_string())
     }
 }
 
+// Map the std::io::Error to the InternalError variant of the AppError
 impl From<IoError> for AppError {
     fn from(e: IoError) -> Self {
         AppError::InternalError(e.to_string())
     }
 }
 
+// Map the std::env::VarError to the InternalError variant of the AppError
 impl From<VarError> for AppError {
     fn from(e: VarError) -> Self {
         AppError::InternalError(e.to_string())
     }
 }
 
+// Map the serde_json::Error to the InternalError variant of the AppError
 impl From<SerdeJsonError> for AppError {
     fn from(e: SerdeJsonError) -> Self {
         AppError::InternalError(e.to_string())
     }
 }
 
+// Map the Solana ParsePubkeyError to the SolanaError variant of the AppError
 impl From<ParsePubkeyError> for AppError {
     fn from(_: ParsePubkeyError) -> Self {
         AppError::SolanaError("Invalid Address".to_string())
     }
 }
 
+// Map the Solana ClientError to the SolanaError variant of the AppError
 impl From<ClientError> for AppError {
     fn from(e: ClientError) -> Self {
         AppError::SolanaError(e.to_string())
--- backend/src/message.rs
+++ backend/src/message.rs
@@ -2,6 +2,8 @@ use serde::Serialize;
 
 use crate::models::{Account, TransactionSignatureCreate};
 
+// Enum that is used in Websocket communication messages
+// Server sends the messages to client based on the IndexingMessage enum variants
 #[derive(Debug, Serialize)]
 #[serde(tag = "type")]
 pub enum IndexingMessage<'a> {
