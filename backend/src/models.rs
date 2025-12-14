3f1e2b4ae9af534aeed6b72c7fba15e0f9c8c93d
Tharun <tharun.damera4@gmail.com>
2025-12-14 17:57:24 +0530

    Create an AddressIndexingState models for having the state of an address during indexing/refreshing and idle

--- backend/src/models.rs
+++ backend/src/models.rs
@@ -3,6 +3,29 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum IndexingState {
+    Idle,
+    Indexing,
+    Syncing,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AddressIndexingState {
+    #[serde(rename = "_id")]
+    pub address: String,
+    pub state: IndexingState,
+    pub created_at: BsonDateTime,
+    pub updated_at: BsonDateTime,
+}
+
+#[derive(Debug)]
+pub struct UpdateAddressIndexingState {
+    pub state: IndexingState,
+    pub updated_at: BsonDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Account {
     #[serde(rename = "_id")]
     pub address: String,
